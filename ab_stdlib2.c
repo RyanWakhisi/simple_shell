89d2aea7b5219567c5a4cfff31b3c6c32660d303
RyanWakhisi <brianwakhisi@gmail.com>
2023-11-15 13:28:09 +0300

    Converts string to integer

---
+++ ab_stdlib2.c
@@ -0,0 +1,36 @@
+
+#include "main.h"
+
+/**
+ * convert_string_to_int - A function that convert a string to an int.
+ * @s: input_str
+ * Return: integer.
+ */
+int convert_string_to_int(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
