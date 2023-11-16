33197606c342f9da20634fbebaa2b2441059ea52
Allan Kipruto <koechallanhi@gmail.com>
2023-11-15 17:01:40 -0800

    main.h cahnges

--- main.h
+++ main.h
@@ -109,4 +109,60 @@ void shell_free_data_held(data_shell *datash);
 int ab_exit_shell(data_shell *datash);
 int ab_exec_line(data_shell *datash);
 int shell_current_dir_checker(char *path, int *i);
+
+char *shell_strconcatcat_cd(data_shell *datash, char *msg, char *error, char *ver_str);
+char *shell_no_err_found(data_shell *datash);
+char *shell_exit_error(data_shell *datash);
+char *shell_env_error(data_shell *datash);
+char *denied_path_persmission(data_shell *datash);
+void shell_builtin_env_help(void);
+void shell_help_env_set(void);
+void unsetenv_shell_help(void);
+void help_builtin_general(void)
+void shell_help_exit(void)
+
+void shell_aux_help(void);
+void shell_help_alias(void);
+void shell_help_cd(void);
+sep_list *shell_add_node_end(sep_list **head, char sep);
+void shell_free_list(sep_list **head);
+void shell_free_list_line(line_list **head);
+r_var *shell_add_rvar_node(r_var **head, int lvar, char *val, int lval);
+void shell_free_list_rvar(r_var **head);
+void pointer_memcpy(void *newptr, const void *ptr, unsigned int size);
+void *pointer_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int shell_get_len(int n);
+char *shell_itoa(int n);
+int shell_atoi(char *s);
+
+char *shell_strcat(char *dest, const char *src);
+char *shell_strcpy(char *dest, char *src);
+int shell_strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+char *shell_strdup(const char *s);
+int shell_strlen(const char *s);
+int shell_cmp_chars(char str[], const char *delim);
+int shell_isdigit(const char *s);
+
+void ab_reverse_string(char *s);
+void ab_cd_dot(data_shell *datash);
+void ab_cd_to(data_shell *datash);
+void cd_previous(data_shell *datash);
+void cd_to_home(data_shell *datash);
+int ab_repeated_char(char *input, int i);
+int ab_error_sep_op(char *input, int i, char last);
+
+int ab_first_char(char *input, int *i);
+void ab_print_syntax_error(data_shell *datash, char *input, int i, int bool);
+int ab_check_error(data_shell *datash, char *input);
+
+int ab_cmp_env_name(const char *nenv, const char *name);
+char *_getenv(const char *name, char **_environ);
+int _env(data_shell *datash);
+
+char *ab_copy_info(char *name, char *value);
+void ab_set_env(char *name, char *value, data_shell *datash);
+int _setenv(data_shell *datash);
+int _unsetenv(data_shell *datash);
 #endif
