76a07080885f5e0f193c5ac96570133458bd71ae
Allan Kipruto <koechallanhi@gmail.com>
2023-11-16 04:24:07 +0300

    sigint

--- brayo_get_sigint.c
+++ brayo_get_sigint.c
@@ -4,7 +4,7 @@
  * shell_get_sigint - Handle the crtl + c call in prompt
  * @sig: Signal handler
  */
-void shell_get_sigint(int sig)
+void get_sigint(int sig)
 {
 	(void)sig;
 	write(STDOUT_FILENO, "\n^-^ ", 5);
