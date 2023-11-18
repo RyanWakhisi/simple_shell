ebc01c2c928d2c27606a0c88406594352aef7d5d
Allan Kipruto <koechallanhi@gmail.com>
2023-11-17 18:19:41 -0800

    final shell

--- execute_command.c
+++ execute_command.c
@@ -1,178 +1,60 @@
-#include "shell.h"
-
+#include "simple_shell.h"
 /**
-* find_path - finds the path of the user input
-* and returns a pointer to the path variable
-* @env: an array of environment variables
-* Return: pointer to the path
-*/
+ * execute_command - function that executes a command
+ * @command: command to be executed
+ */
+void execute_command(const char *command, char **env) {
+    pid_t child_pid = fork();
 
-char *find_path(char **env)
-{
-	char *path = NULL;
-	int i = 0;
+    if (child_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0) {
+        char *args[128];
+        int arg_count = 0;
+        char *token = strtok((char *)command, " ");
 
-	while (env[i])
-	{
-		if (_strstr(env[i], "PATH") && env[i][0] == 'P' && env[i][4] == '=')
-		{
-			path = env[i];
-			break;
-		}
-		i++;
-	}
-	if (path != NULL)
-	{
-		for (i = 0; i < 6; i++)
-		path++;
-	}
-	return (path);
-}
+        while (token != NULL) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_count] = NULL;
 
-/**
-* split_path - splits the path
-* @path: path found and to be splitted
-* Return: an array of splitted path
-*/
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] != NULL) {
+                if (chdir(args[1]) != 0) {
+                    perror("chdir");
+                }
+            } else {
+                if (chdir(getenv("HOME")) != 0) {
+                    perror("chdir");
+                }
+            }
+            exit(EXIT_SUCCESS);
+        }
 
-char **split_path(char *path)
-{
-	char **tokens = NULL, *stoks, *path_copy = NULL;
-	int i = 0, num_tokens = 0;
+        if (strchr(args[0], '/') == NULL) {
+        	char *path_token;
+            char *path = getenv("PATH");
+            if (path == NULL) {
+                path = "/usr/bin";
+            }
 
-	path_copy = _strdup(path);
-	stoks = strtok(path_copy, ":");
-	while (stoks != NULL)
-		num_tokens++, stoks = strtok(NULL, ":");
-	tokens = malloc(sizeof(char *) * (num_tokens + 1));
-	if (tokens == NULL)
-	{
-		perror("Error tokenizing");
-		free(path_copy);
-		return (NULL);
-	}
-	free(path_copy);
-	path_copy = _strdup(path);
-	stoks = strtok(path_copy, ":");
-	for (i = 0; i < num_tokens; i++)
-	{
-		tokens[i] = _strdup(stoks);
-		stoks = strtok(NULL, ":");
-	}
-	tokens[i] = NULL;
-	free(path_copy);
-	return (tokens);
-}
+            path_token = strtok(path, ":");
+            while (path_token != NULL) {
+                char full_path[256];
+                snprintf(full_path, sizeof(full_path), "%s/%s", path_token, args[0]);
+                execve(full_path, args, env);
+                path_token = strtok(NULL, ":");
+            }
+        } else {
+            execve(args[0], args, env);
+        }
 
-/**
-* execute_command - executes all the commands inputted
-* @args: an array of strings inputted
-* @env: an array of environment variables
-*/
-
-void execute_command(char **args, char **env)
-{
-	pid_t pid;
-	int status;
-	char *path;
-
-	if (is_builtin(args, env))
-		execute_builtin(args, env);
-	else
-	{
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("Forking failed"), free_args(args);
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (args[0] != NULL && _strstr(args[0], "/") != NULL)
-			{
-				if (execve(args[0], args, env) == -1)
-					perror("Execution failed"),  free_args(args), exit(EXIT_FAILURE);
-			}
-			else
-			{
-				path = find_command(args[0], env);
-				if (path != NULL)
-				{
-					if (execve(path, args, env) == -1)
-						perror("Exec failed"), free(path), free_args(args), exit(EXIT_FAILURE);
-				}
-				else
-				{
-					perror("Command not found"), free(path), free_args(args);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-		else
-			wait(&status), errno = status;
-	}
-}
-
-/**
-* find_command - finds the command for relative path
-* @command: an array of strings inputted
-* @env: an array of environmant variables
-* Return: full path
-*/
-
-char *find_command(char *command, char **env)
-{
-	char *path_env = find_path(env), **paths, *full_path = NULL;
-	int i = 0;
-
-	if (path_env != NULL)
-	{
-		paths = split_path(path_env);
-		for (i = 0; paths[i] != NULL; i++)
-		{
-			if  (paths[i] != NULL && command != NULL)
-			{
-				full_path = malloc(strlen(paths[i]) + strlen(command) + 2);
-				if (full_path != NULL)
-				{
-					sprintf(full_path, "%s/%s", paths[i], command);
-					if (access(full_path, X_OK) == 0)
-					{
-						free_tokens(paths);
-						return (full_path);
-					}
-					free(full_path);
-				}
-				else
-				{
-					perror("Memory allocation failed");
-					free_tokens(paths);
-					return (NULL);
-				}
-			}
-			else
-			{
-				perror("Error: path or command is empty");
-			}
-		}
-	}
-	free_tokens(paths);
-	return (NULL);
-}
-
-/**
-* free_tokens - frees tokens
-* @tokens: a pointer to a pointer to an array of tokens
-*/
-
-void free_tokens(char **tokens)
-{
-	int i = 0;
-
-	while (tokens[i] != NULL)
-	{
-		free(tokens[i]);
-		i++;
-	}
-	free(tokens);
+        perror("execve");
+        exit(EXIT_FAILURE);
+    } else {
+        int status;
+        wait(&status);
+    }
 }
